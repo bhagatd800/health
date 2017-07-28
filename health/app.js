6c4445ffed66f820d930f5981a15b3c8a26b4da6
kd33pK <bhagatd800@gmail.com>
2017-07-28 13:01:10 +0530

    Update app.js

--- health/app.js
+++ health/app.js
@@ -118,4 +118,24 @@ app.use(function(err, req, res, next) {
 
 });
 
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 module.exports = app;
